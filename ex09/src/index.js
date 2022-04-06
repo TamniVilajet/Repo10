ae392b7b51a6b3d953f2d61304278dcd6f60960a
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-06 22:57:34 +0200

    Exercise 09

--- ex09/src/index.js
+++ ex09/src/index.js
@@ -8,7 +8,7 @@ function myFunction(myObj, checkProp) {
 
     // Only change code above this line
     }
-    console.log(myFunction( {title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
+    console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "title"));
     console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "song"));
     console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "genre"));
     console.log(myFunction({title: "Titanic", song: "My Heart Will Go On", genre: "drama"}, "actor"));
