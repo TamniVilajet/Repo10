aadc54751e4ec384463d8b1c81ef12b5332bde76
TamniVilajet <ermin.kirlic@gmail.com>
2022-04-06 14:34:42 +0200

    Exercise 01

---
+++ ex01/src/index.js
@@ -0,0 +1,15 @@
+// Only change code below this lin
+var myPet = {
+    species: "Cho Cowh",
+    name: "Baloo",
+    legs: 4,
+    friends: ["Brandy","Luna"]
+}
+
+
+function myFunction(obj) {
+    return obj;
+}
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction};
